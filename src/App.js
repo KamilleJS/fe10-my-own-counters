b9c35ca3d0fb721d127cec85d312bf06c9c038a9
KamilleJS <jkuklinaj@gmail.com>
2021-02-26 12:01:31 -0800

    minus one

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Button } from 'reactstrap';
 
 const initialCount = [
-  {value: 5, id: 1}
+    {value: 5, id: 1}
 ]
 
 function App() {
 
-  const [counts, setCounts] = useState(initialCount);
+    const [counts, setCounts] = useState(initialCount);
 
-  return (
-    <div>
-      <h2>Counters</h2>
+    const minusOne = (countId) => {
+        const newCounts = counts.map((el) => {
+            if (el.id === countId) {
+                el.value--;
+            }
+            return el;
+        })
+        setCounts(newCounts);
+    }
 
-      {counts.map(el => <li key={el.id}>
-        <Button color="secondary" size="sm">-1</Button>
-        {' '}
-        {el.value}
-        {' '}
-        <Button color="secondary" size="sm">+1</Button>
-        {' '}
-        <Button outline color="primary" size="sm">Remove counter</Button>
-      </li>)}
-        <br/>
-      <Button outline color="success" size="sm">Add counter</Button>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Counters</h2>
+            {counts.map((el) =>
+                <li key={el.id}>
+                    <button onClick={() => minusOne(el.id)} type="button" className="btn btn-secondary btn-sm">-1</button>
+                    {' '}
+                    {el.value}
+                    {' '}
+                    <button type="button" className="btn btn-secondary btn-sm">+1</button>
+                    {' '}
+                    <button type="button" className="btn btn-secondary btn-sm">Remove counter</button>
+                </li>)}
+            <button type="button" className="btn btn-secondary btn-sm">Add counter</button>
+        </div>
+    );
 }
 
 export default App;
