d7a1c809e0dc105973afbab1dd77664f4144abe0
KamilleJS <jkuklinaj@gmail.com>
2021-02-26 11:30:59 -0800

    buttons, reactstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap';
 
 const initialCount = [
   {value: 5, id: 1}
@@ -13,6 +15,8 @@ function App() {
       <h2>Counters</h2>
 
       {counts.map(el => <li key={el.id}>
+        <Button color="secondary" size="sm">-1</Button>
+        {' '}
         {el.value}
       </li>)}
     </div>
