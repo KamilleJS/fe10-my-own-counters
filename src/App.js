efd165de808ae96c3623ad11d5f97732a129710c
KamilleJS <jkuklinaj@gmail.com>
2021-02-26 12:17:14 -0800

    result

--- src/App.js
+++ src/App.js
@@ -11,12 +11,30 @@ function App() {
 
     const minusOne = (countId) => {
         const newCounts = counts.map((el) => {
-            if (el.id === countId) {
-                el.value--;
-            }
+            if (el.id === countId){el.value--;}
             return el;
         })
         setCounts(newCounts);
+    }
+
+    const plusOne = (countId) => {
+        const newCounts = counts.map((el) => {
+            if(el.id === countId){el.value++;}
+            return el;
+        })
+        setCounts(newCounts);
+    }
+
+    const removeCounter = (countId) => {
+        const newCounts = counts.filter(el => {
+            if(el.id !== countId){return el;}
+        })
+        setCounts(newCounts);
+    }
+
+    const addCounter = () => {
+        const newCounts = Math.floor(Math.random()*10);
+        setCounts([...counts, {value: newCounts, id: counts.length}]);
     }
 
     return (
@@ -28,11 +46,11 @@ function App() {
                     {' '}
                     {el.value}
                     {' '}
-                    <button type="button" className="btn btn-secondary btn-sm">+1</button>
+                    <button onClick={() => plusOne(el.id)} type="button" className="btn btn-secondary btn-sm">+1</button>
                     {' '}
-                    <button type="button" className="btn btn-secondary btn-sm">Remove counter</button>
+                    <button onClick={() => removeCounter(el.id)} type="button" className="btn btn-secondary btn-sm">Remove counter</button>
                 </li>)}
-            <button type="button" className="btn btn-secondary btn-sm">Add counter</button>
+            <button onClick={() => addCounter()} type="button" className="btn btn-secondary btn-sm">Add counter</button>
         </div>
     );
 }
